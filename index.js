0150f9f40ea1f4a9767ba6addf851493953282f2
hempi <115639626+VladislavBukirev@users.noreply.github.com>
2024-04-15 12:46:20 +0500

    task3-5

--- index.js
+++ index.js
@@ -14,8 +14,39 @@ document.querySelector(".add-button").addEventListener("click", () => {
             newForm.remove();
             count--;
         }
-
     });
     forms[forms.length - 1].after(newForm);
 });
+
+document.querySelector('.submit-button').addEventListener('click', (event) => {
+    event.preventDefault();
+    openModal();
+});
+
+document.querySelector('.close-model').addEventListener('click', () => {
+    closeModal();
+});
+
+function openModal() {
+    const overlay = document.querySelector('.overlay');
+    overlay.style.visibility = 'visible';
+    document.querySelector('.status-order').textContent = updateBeverageCount();
+}
+
+function updateBeverageCount() {
+    const num = document.querySelectorAll('fieldset').length;
+    const mod = num % 10;
+    return `Заказ принят! Вы заказали ${num} ${num !== 11 && mod === 1 ?
+        'напиток'
+        : (num > 20 && (mod === 2 || mod === 3 || mod === 4)) || (num >= 2 && num <= 4) ?
+            'напитка'
+            : 'напитков'}`;
+}
+
+function closeModal() {
+    const overlay = document.querySelector('.overlay');
+    overlay.style.visibility = 'hidden';
+}
+
+
 
